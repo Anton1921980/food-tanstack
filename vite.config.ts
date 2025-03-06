eb569971b50255216dfa18010eb09843a351dcdb
Anton1921980 <mywear1@gmail.com>
2025-03-06 23:57:09 +0200

    fix: vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vituumPostcss from '@vituum/vite-plugin-postcss'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vituumPostcss(),
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'skip-type-check',
+          transform(code, id) {
+            if (id.endsWith('.ts') || id.endsWith('.tsx')) {
+              return {
+                code,
+                map: null // Skip sourcemap generation
+              };
+            }
+          }
+        }
+      ]
+    }
+  }
 })
