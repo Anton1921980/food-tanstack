f6a0d44a986f8e4dc8c076d0b52f1f3ffab9972b
Anton1921980 <mywear1@gmail.com>
2025-03-06 23:59:07 +0200

    fix: tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vituumPostcss from '@vituum/vite-plugin-postcss'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vituumPostcss(),
-  ],
-  build: {
-    rollupOptions: {
-      plugins: [
-        {
-          name: 'skip-type-check',
-          transform(code, id) {
-            if (id.endsWith('.ts') || id.endsWith('.tsx')) {
-              return {
-                code,
-                map: null // Skip sourcemap generation
-              };
-            }
-          }
-        }
-      ]
-    }
-  }
+  plugins: [react()],
 })
